61af64c474caa2da7c37f6c99c98319d090cd62c
sigmko <44901543+sigmko@users.noreply.github.com>
2019-10-01 00:30:03 -0400

    style: remove unused files and methods

--- app/src/main/java/com/atmko/onmywatch/utils/network_utils/NetworkFunctions.java
+++ app/src/main/java/com/atmko/onmywatch/utils/network_utils/NetworkFunctions.java
@@ -23,27 +23,6 @@ import java.net.Socket;
 import java.net.SocketAddress;
 
 public class NetworkFunctions {
-    private static final String SOCKET_ADDRESS = "8.8.8.8";
-    private static final int PORT_NUMBER = 53;
-    private static final int TIMEOUT_MILLIS = 1500;
-
-    //source: https://stackoverflow.com/questions/1560788/how-to-check-internet-access-on-android-inetaddress-never-times-out
-    //user: Levit
-    //date: Dec 5 '14
-    public static boolean isOnline() {
-        try {
-            Socket sock = new Socket();
-            SocketAddress socketAddress = new InetSocketAddress(SOCKET_ADDRESS, PORT_NUMBER);
-
-            sock.connect(socketAddress, TIMEOUT_MILLIS);
-            sock.close();
-
-            return true;
-        } catch (IOException e) {
-            return false;
-        }
-    }
-
     //agnostic search request
     public static ANRequest agnosticSearchRequest(String urlFormat,
                                                   SearchPreferences searchPreferences,
