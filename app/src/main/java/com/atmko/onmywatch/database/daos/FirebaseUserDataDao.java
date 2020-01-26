bc1abfa607103c7422f4a76ce00792ccda2476ba
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-01-25 19:55:03 -0500

    refactor: update path to get user_tier

--- app/src/main/java/com/atmko/onmywatch/database/daos/FirebaseUserDataDao.java
+++ app/src/main/java/com/atmko/onmywatch/database/daos/FirebaseUserDataDao.java
@@ -12,7 +12,12 @@ import com.google.firebase.firestore.DocumentReference;
  */
 
 public class FirebaseUserDataDao {
+    private static final String ADMIN_COLLECTION_TITLE = "admin";
+    private static final String PROFILE_INFO_DOCUMENT_TITLE = "profile_info";
+
     public static DocumentReference getUserTier() {
-        return MasterActivity.getUserDbHomeReference();
+        return MasterActivity.getUserDbHomeReference()
+                .collection(ADMIN_COLLECTION_TITLE)
+                .document(PROFILE_INFO_DOCUMENT_TITLE);
     }
 }
