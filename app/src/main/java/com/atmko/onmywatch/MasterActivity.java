f2d8ff37ef17bd0ee27c792e231736e720cfef10
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-03-27 17:35:44 -0400

    rollback: loading last backup on database init
    remove loading last backup on database initialized

--- app/src/main/java/com/atmko/onmywatch/MasterActivity.java
+++ app/src/main/java/com/atmko/onmywatch/MasterActivity.java
@@ -38,8 +38,6 @@ import com.atmko.onmywatch.Fragments.ListResultsParentFragment;
 import com.atmko.onmywatch.Fragments.PeopleDetailsFragment;
 import com.atmko.onmywatch.custom_views.SuperEditText;
 import com.atmko.onmywatch.database.AppDatabase;
-import com.atmko.onmywatch.database.daos.FirebaseUserDataDao;
-import com.atmko.onmywatch.models.Backup;
 import com.atmko.onmywatch.models.MediaData;
 import com.atmko.onmywatch.models.MediaNotifier;
 import com.atmko.onmywatch.models.MovieData;
@@ -50,7 +48,6 @@ import com.atmko.onmywatch.utils.api_utils.NetworkFunctions;
 import com.atmko.onmywatch.utils.api_utils.SearchPreferences;
 import com.atmko.onmywatch.utils.network_utils.AppExecutors;
 import com.atmko.onmywatch.utils.network_utils.BackupService;
-import com.atmko.onmywatch.utils.network_utils.RestoreService;
 import com.atmko.onmywatch.utils.network_utils.work_manager_workers.BackupWorker;
 import com.atmko.onmywatch.utils.network_utils.work_manager_workers.UpdateMediaWorker;
 import com.atmko.onmywatch.view_models.MasterActivityViewModel;
@@ -819,7 +816,6 @@ public class MasterActivity extends AppCompatActivity
     }
 
     public RoomDatabase.Callback databaseInitializer(final Context context) {
-        final boolean[] isRestored = {false};
         //reference
         //https://medium.com/@srinuraop/database-create-and-open-callbacks-in-room-7ca98c3286ab
         return new RoomDatabase.Callback() {
@@ -828,8 +824,6 @@ public class MasterActivity extends AppCompatActivity
                 AppExecutors.getInstance().diskIO().execute(new Runnable() {
                     @Override
                     public void run() {
-
-
                         String[] seriesWatchListTitles = context.getResources()
                                 .getStringArray(R.array.watch_status_series_titles);
                         for (String title: seriesWatchListTitles) {
@@ -837,11 +831,6 @@ public class MasterActivity extends AppCompatActivity
                             AppDatabase.getInstance(context).watchListsDao()
                                     .addList(watchListModel);
                         }
-
-                        restoreLatestBackup();
-
-                        //prevent restoring again if database opens
-                        isRestored[0] = true;
                     }
                 });
             }
@@ -849,30 +838,8 @@ public class MasterActivity extends AppCompatActivity
             @Override
             public void onOpen(@NonNull SupportSQLiteDatabase db) {
                 super.onOpen(db);
-                AppExecutors.getInstance().diskIO().execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        //check if database has been restored already (from onCreate method)
-                        if (!isRestored[0]) {
-                            restoreLatestBackup();
-                        }
-                    }
-                });
             }
         };
-    }
-
-    private void restoreLatestBackup() {
-        List<Backup> backups = FirebaseUserDataDao.getBackupsAlt();
-        if (backups.size() != 0) {
-            Backup latestBackup = backups.get(0);
-
-            //restore backup;
-            Intent intent = new Intent(this, RestoreService.class);
-            intent.putExtra(RestoreService.FOLDER_KEY, RestoreService.BACKUP_FOLDER_NAME);
-            intent.putExtra(RestoreService.FILENAME_KEY, latestBackup.getFileName());
-            RestoreService.enqueueWork(this, intent);
-        }
     }
 
     /**
