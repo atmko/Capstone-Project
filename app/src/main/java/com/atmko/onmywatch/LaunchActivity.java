f77827e5d6dc8c192f41dc3b5e41eaa968fc4558
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-04-05 17:55:39 -0400

    feat: add clickable span to terms text view

--- app/src/main/java/com/atmko/onmywatch/LaunchActivity.java
+++ app/src/main/java/com/atmko/onmywatch/LaunchActivity.java
@@ -1,16 +1,23 @@
 package com.atmko.onmywatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatCheckBox;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LaunchActivity extends AppCompatActivity {
     public static final String APPLICATION_PREFERENCE_KEY = "application";
@@ -35,6 +42,30 @@ public class LaunchActivity extends AppCompatActivity {
 
     private void defineViews() {
         agreementErrorTextView = findViewById(R.id.agreement_error_text_view);
+
+        TextView termsTextView = findViewById(R.id.terms_text_view);
+        SpannableString spannableString = new SpannableString(termsTextView.getText().toString());
+        ClickableSpan termsClickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                Toast.makeText(LaunchActivity.this, "launch terms", Toast.LENGTH_SHORT).show();
+            }
+        };
+        ClickableSpan privacyClickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                Toast.makeText(LaunchActivity.this, "launch privacy", Toast.LENGTH_SHORT).show();
+            }
+        };
+        int[] clickableSpans = getResources().getIntArray(R.array.terms_clickable_spans);
+        spannableString.setSpan(termsClickableSpan, clickableSpans[0],
+                clickableSpans[1], Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(privacyClickableSpan, clickableSpans[2],
+                clickableSpans[3], Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        termsTextView.setText(spannableString);
+        termsTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        termsTextView.setHighlightColor(Color.TRANSPARENT);
+
         checkBox = findViewById(R.id.i_understand_checkbox);
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
