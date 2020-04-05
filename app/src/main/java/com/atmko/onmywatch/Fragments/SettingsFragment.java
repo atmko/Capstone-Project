98837462ed08db15689176591744cf9a452a1590
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-04-05 19:09:15 -0400

    feat: add use terms and privacy policy to settings

--- app/src/main/java/com/atmko/onmywatch/Fragments/SettingsFragment.java
+++ app/src/main/java/com/atmko/onmywatch/Fragments/SettingsFragment.java
@@ -5,17 +5,19 @@
 package com.atmko.onmywatch.Fragments;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
-import com.atmko.onmywatch.RestoreActivity;
 import com.atmko.onmywatch.ConfirmationActivity;
 import com.atmko.onmywatch.MasterActivity;
 import com.atmko.onmywatch.R;
+import com.atmko.onmywatch.RestoreActivity;
 import com.atmko.onmywatch.utils.network_utils.AppExecutors;
 import com.atmko.onmywatch.utils.network_utils.BackupService;
 
@@ -65,6 +67,42 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                     return true;
                 }
             });
+        }
+
+        Preference privacyPreference = findPreference(getString(R.string.settings_key_privacy));
+        if (privacyPreference != null) {
+            privacyPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(final Preference preference) {
+                    launchBrowserIntent(getString(R.string.privacy_url));
+                    return true;
+                }
+            });
+        }
+
+        Preference termsPreference = findPreference(getString(R.string.settings_key_terms));
+        if (termsPreference != null) {
+            termsPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(final Preference preference) {
+                    launchBrowserIntent(getString(R.string.terms_url));
+                    return true;
+                }
+            });
+        }
+    }
+
+    private void launchBrowserIntent(String url) {
+        Uri webPage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webPage);
+        if (getActivity() != null) {
+            if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                startActivity(intent);
+
+            } else {
+                Toast.makeText(getContext(), getString(R.string.no_browser_error_message),
+                        Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
