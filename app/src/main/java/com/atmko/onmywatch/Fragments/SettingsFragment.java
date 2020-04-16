f9aff0cf80a66323a98b82d55e3766fcc2d9fa69
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-04-16 06:55:44 -0400

    feat: convert anonymous user to permanent user

--- app/src/main/java/com/atmko/onmywatch/Fragments/SettingsFragment.java
+++ app/src/main/java/com/atmko/onmywatch/Fragments/SettingsFragment.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
@@ -20,15 +21,50 @@ import com.atmko.onmywatch.R;
 import com.atmko.onmywatch.RestoreActivity;
 import com.atmko.onmywatch.utils.network_utils.AppExecutors;
 import com.atmko.onmywatch.utils.network_utils.BackupService;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import static android.app.Activity.RESULT_OK;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
     private static final int REQUEST_CREATE_BACKUP = 1;
+    private static final int UPGRADE_TO_PERMANENT_ID = 2;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_settings);
+
+        Preference linkPreference = findPreference(getString(R.string.settings_key_link));
+        if (linkPreference != null) {
+            linkPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(final Preference preference) {
+                    if (MasterActivity.getCurrentUser().isAnonymous()) {
+                        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (getActivity() != null) {
+                                    googleSignInToPermanentAccount();
+                                }
+                            }
+                        });
+                    } else {
+                        Toast.makeText(getContext(), "Account Already Linked",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    return true;
+                }
+            });
+        }
 
         Preference restorePreference = findPreference(getString(R.string.settings_key_restore));
         if (restorePreference != null) {
@@ -117,6 +153,73 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                     BackupService.enqueueWork(getContext(), intent);
                 }
             }
+        } else if (requestCode == UPGRADE_TO_PERMANENT_ID) {
+            if (resultCode == RESULT_OK) {
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                handleSignInResult(task);
+
+            } else {
+                Toast.makeText(getContext(), "Failed To Link Account", Toast.LENGTH_SHORT).show();
+                signOutFromGoogle();
+            }
+        }
+    }
+
+    private void googleSignInToPermanentAccount() {
+        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        if (getContext() == null) return;
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(getContext(), options);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, UPGRADE_TO_PERMANENT_ID);
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> task) {
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            if (account != null) convertAccountToPermanent(account);
+
+        } catch (ApiException e) {
+            Toast.makeText(getContext(), "Failed To Sign In", Toast.LENGTH_SHORT).show();
+            signOutFromGoogle();
+        }
+    }
+
+    private void convertAccountToPermanent(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        if (MasterActivity.getCurrentUser() != null && getActivity() != null) {
+            MasterActivity.getCurrentUser().linkWithCredential(credential)
+                    .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getContext(), "Account Successfully Linked.",
+                                        Toast.LENGTH_SHORT).show();
+                                signOutFromGoogle();
+
+                            } else {
+                                if (task.getException() != null) {
+                                    Toast.makeText(getContext(), task.getException().getMessage(),
+                                            Toast.LENGTH_SHORT).show();
+                                }
+
+                                signOutFromGoogle();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void signOutFromGoogle() {
+        if (getActivity() != null) {
+            GoogleSignInOptions gso =
+                    new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                            .requestEmail()
+                            .build();
+            GoogleSignIn.getClient(getActivity(), gso).signOut();
         }
     }
 }
