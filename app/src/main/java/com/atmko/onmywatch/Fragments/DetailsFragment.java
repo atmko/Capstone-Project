atmko/Capstone-Project

commit fa82be31c630f7d7471da42444dd06fe71f9f40c

refactor: remove details set text logic to model

app/src/main/java/com/atmko/onmywatch/Fragments/DetailsFragment.java
@@ -439,8 +439,8 @@ public class DetailsFragment extends Fragment {
                 mWatchStatus = mediaData != null ? castedMediaData.getWatchStatus() : 0;
 
                 //get array of watch status title shorthand
-                String[] watchStatusShorthandList =
-                        getContext().getResources().getStringArray(R.array.watch_status_shorthand_titles);
+                String[] watchStatusShorthandList = getContext().getResources()
+                                .getStringArray(R.array.watch_status_shorthand_titles);
 
                 //get shorthand using watch status as index
                 String shorthand = mediaData != null
@@ -456,7 +456,8 @@ public class DetailsFragment extends Fragment {
                 int userRating = mediaData != null ? castedMediaData.getUserRating() : 0;
                 if (userRating != 0) {
                     userRatingTextView.setVisibility(View.VISIBLE);
-                    userRatingTextView.setText(userRating + ".0");
+                    String userRatingString = userRating + ".0";
+                    userRatingTextView.setText(userRatingString);
 
                 } else {
                     userRatingTextView.setText(getString(R.string.rate_text_literal));
@@ -635,13 +636,7 @@ public class DetailsFragment extends Fragment {
     private void setMovieCountDown(MovieData movieData) {
         //set count down if available
         ScheduledMedia releaseMedia = movieData.getScheduledMedia();
-        String countdown;
-        if (releaseMedia != null && releaseMedia.getCountdown() != null) {
-            countdown = releaseMedia.getCountdown();
-
-        } else {
-            countdown = ScheduledMedia.NO_DATES;
-        }
+        String countdown = releaseMedia.getCountdown();
 
         mCountDownTextView.setText(countdown);
         mCountDownTextView.setVisibility(View.VISIBLE);
@@ -650,13 +645,7 @@ public class DetailsFragment extends Fragment {
     private void setSeriesCountDown(SeriesData seriesData) {
         //set count down if available
         Episode nextEpisode = seriesData.getNextEpisodeToAir();
-        String countdown;
-        if (nextEpisode != null && nextEpisode.getCountdown() != null) {
-            countdown = nextEpisode.getCountdown();
-
-        } else {
-            countdown = ScheduledMedia.NO_DATES;
-        }
+        String countdown = nextEpisode.getCountdown();
 
         mCountDownTextView.setText(countdown);
         mCountDownTextView.setVisibility(View.VISIBLE);
@@ -803,25 +792,15 @@ public class DetailsFragment extends Fragment {
         ((TextView) getView().findViewById(R.id.title_text_view))
                 .setText(mMediaData.getFormattedTitle());
 
-        if (!mMediaData.getReleaseDate().equals("")) {
-            ((TextView) getView().findViewById(R.id.date_text_view)).setText(
-                    GeneralUtils.parseDateToYear(mMediaData.getReleaseDate()));
-
-        } else {
-            ((TextView) getView().findViewById(R.id.date_text_view)).setText(
-                    getString(R.string.release_date_placeholder));
-        }
+        //set date text
+        ((TextView) getView().findViewById(R.id.date_text_view)).setText(
+                GeneralUtils.parseDateToYear(mMediaData.getReleaseDate()));
 
         ((TextView) getView().findViewById(R.id.rating_text_view)).setText(mMediaData.getVoteAverage());
 
-        try {
-            //set overview text
-            ((TextView) getView().findViewById(R.id.overView_text_view)).setText(
-                    limitText(mMediaData.getOverview(), mOverviewCutoffIndex));
-
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
+        //set overview text
+        ((TextView) getView().findViewById(R.id.overView_text_view)).setText(
+                limitText(mMediaData.getOverview(), mOverviewCutoffIndex));
     }
 
     // TODO: NullPointerException handled in caller
@@ -900,8 +879,7 @@ public class DetailsFragment extends Fragment {
                         .setText(genres.get(0));
 
             } else {
-                getView().findViewById(R.id.genre_0_text_view)
-                        .setVisibility(View.GONE);
+                getView().findViewById(R.id.genre_0_text_view).setVisibility(View.GONE);
             }
 
             if (genres.size() > 1) {
@@ -909,8 +887,7 @@ public class DetailsFragment extends Fragment {
                         .setText(genres.get(1));
 
             } else {
-                getView().findViewById(R.id.genre_1_text_view)
-                        .setVisibility(View.GONE);
+                getView().findViewById(R.id.genre_1_text_view).setVisibility(View.GONE);
             }
         }
     }

app/src/main/java/com/atmko/onmywatch/models/MediaData.java
@@ -5,7 +5,6 @@
 package com.atmko.onmywatch.models;
 
 import android.content.Context;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -39,6 +38,8 @@ abstract public class MediaData {
     public static final String TAGS_KEY = "tags";
 
     private static final String MATURITY_RATING_PLACEHOLDER = "____";
+    private static final String RELEASE_DATE_PLACEHOLDER = "____";
+    private static final String NO_STATUS_PLACEHOLDER = "No Status";
 
     //primary attributes
     @PrimaryKey
@@ -130,11 +131,21 @@ abstract public class MediaData {
     }
 
     public String getOverview() {
-        return mOverview;
+        if (mOverview != null && !mOverview.equals("")) {
+            return mOverview;
+
+        } else {
+            return "";
+        }
     }
 
     public String getReleaseDate() {
-        return mReleaseDate;
+        if (mReleaseDate != null && !mReleaseDate.equals("")) {
+            return mReleaseDate;
+
+        } else {
+            return RELEASE_DATE_PLACEHOLDER;
+        }
     }
 
     public void createTags() {
@@ -183,7 +194,8 @@ abstract public class MediaData {
     }
 
     public String getReleaseStatus() {
-        return mReleaseStatus;
+        return mReleaseStatus != null && !mReleaseStatus.equals("")
+                ? mReleaseStatus : NO_STATUS_PLACEHOLDER;
     }
 
     public void setReleaseStatus(String releaseStatus) {

commit afe683b89355c8604f5862ae386d078f93199072

fix: error caused by commit previous commit
fix DetailsFragment set text errors caused by
fa82be31c630f7d7471da42444dd06fe71f9f40c

app/src/main/java/com/atmko/onmywatch/Fragments/DetailsFragment.java
@@ -636,7 +636,7 @@ public class DetailsFragment extends Fragment {
     private void setMovieCountDown(MovieData movieData) {
         //set count down if available
         ScheduledMedia releaseMedia = movieData.getScheduledMedia();
-        String countdown = releaseMedia.getCountdown();
+        String countdown = releaseMedia != null ? releaseMedia.getCountdown() : ScheduledMedia.NO_DATES;
 
         mCountDownTextView.setText(countdown);
         mCountDownTextView.setVisibility(View.VISIBLE);
@@ -645,7 +645,7 @@ public class DetailsFragment extends Fragment {
     private void setSeriesCountDown(SeriesData seriesData) {
         //set count down if available
         Episode nextEpisode = seriesData.getNextEpisodeToAir();
-        String countdown = nextEpisode.getCountdown();
+        String countdown = nextEpisode != null ? nextEpisode.getCountdown() : ScheduledMedia.NO_DATES;
 
         mCountDownTextView.setText(countdown);
         mCountDownTextView.setVisibility(View.VISIBLE);
@@ -790,11 +790,11 @@ public class DetailsFragment extends Fragment {
                 ((ImageView) getView().findViewById(R.id.backdrop_image_view)));
 
         ((TextView) getView().findViewById(R.id.title_text_view))
-                .setText(mMediaData.getFormattedTitle());
+                .setText(mMediaData.getTitle());
 
         //set date text
         ((TextView) getView().findViewById(R.id.date_text_view)).setText(
-                GeneralUtils.parseDateToYear(mMediaData.getReleaseDate()));
+                GeneralUtils.parseDateToYear(mMediaData.getDisplayReleaseDate()));
 
         ((TextView) getView().findViewById(R.id.rating_text_view)).setText(mMediaData.getVoteAverage());
 

app/src/main/java/com/atmko/onmywatch/models/MediaData.java
@@ -94,10 +94,6 @@ abstract public class MediaData {
         return mTitle;
     }
 
-    public String getFormattedTitle() {
-        return mTitle;
-    }
-
     public double getPopularity() {
         return mPopularity;
     }
@@ -140,6 +136,10 @@ abstract public class MediaData {
     }
 
     public String getReleaseDate() {
+        return mReleaseDate;
+    }
+
+    public String getDisplayReleaseDate() {
         if (mReleaseDate != null && !mReleaseDate.equals("")) {
             return mReleaseDate;
 